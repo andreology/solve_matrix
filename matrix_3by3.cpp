1f2086f4eb30210d884a2d67cf6a56826727bad0
andreology <37252145+andreology@users.noreply.github.com>
2020-02-11 11:20:17 -0800

    creating a matrix structure using C++ and functions to display and input

--- matrix_3by3.cpp
+++ matrix_3by3.cpp
@@ -1,24 +1,55 @@
-#include <stdio.h>
+//a class to represent a a matrix
+#include <iostream>
+using namespace std;
 
-struct Node {
-  int coefficient;
-  int exponent;
-  struct Node *next;
-}*polynomial=NULL;
+class MatrixElement {
+  public:
+    int i, j, x;
+};
 
-void create() {
-  struct Node *t, *last;
-  int num, i;
+class SparseMatrix {
+  private:
+    int m, n, number;
+    MatrixElement *elem;
+  public:
+    //Constructor
+    SparseMatrix(int m, int n, int number)
+    {
+      this -> m = m;
+      this -> n = n;
+      this -> number = number;
+      elem = new MatrixElement[this -> number];
+    }
+    //Destructor
+    ~SparseMatrix() {
+      delete [] elem;
+    }
+    void readElements() {
+      //collecting matrix values from user
+      cout << "Input Elements not equal to zero";
+      for(int i = 0; i < number; i++)
+        cin >> elem[i].i >> elem[i].j >> elem[i].x;
+    }
+    void displayMatrix() {
+      //variable to keep track of non zero elements
+      int k = 0;
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          if(elem[k].i == i && elem[k].j == j)
+          cout << elem[k++].x << " ";
+          else
+            cout << "0 ";
+            //for zero elemetns
+        }
+        cout << endl;
+      }
+    }
+};
 
-  for(i = 0; i < num; i++) {
-    t = (struct Node *)mallac(sizeof(struct Node));
-    scanf("%d%d", &t -> coefficient, &t -> exponent);
-    //linking to link list
-    t -> next = NULL;
-    if(polynomial)
-  }
-}
-
+//main function to test matrix
 int main() {
+  SparseMatrix matrix(5, 5, 5);
+  matrix.readElements();
+  matrix.displayMatrix();
   return 0;
 }
